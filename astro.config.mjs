efcb9ee28bec0b8c556f3dfa9be188ccdae9e354
maxcyberspace <maxwrites02@gmail.com>
2026-06-18 17:11:37 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://YOUR_USERNAME.github.io",
-  base: "/YOUR_REPOSITORY_NAME",
+  site: "https://maxcyberspace.github.io/onestopsite.github.io/",
+  base: "/onestopsite",
   vite: {
     plugins: [tailwindcss()]
   }
